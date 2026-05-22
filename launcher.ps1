b64455f4cc0d687a17d05defbfe1d9c4ca635f2d
hartkitsak <9pkclaymore@gmail.com>
2026-05-22 23:43:01 +0700

    Fix username in launcher and add README

--- launcher.ps1
+++ launcher.ps1
@@ -1,6 +1,6 @@
 $tmp = "$env:TEMP\HksUtil"
 mkdir $tmp -Force | Out-Null
 $zip = "$tmp\repo.zip"
-iwr -Uri "https://github.com/YOUR_USER/HksUtil/archive/main.zip" -OutFile $zip
+iwr -Uri "https://github.com/hartkitsak/HksUtil/archive/main.zip" -OutFile $zip
 Expand-Archive $zip -DestinationPath $tmp -Force
 & "$tmp\HksUtil-main\app.ps1"
